395ea7d74a95f29209a4edc3a1dc091995042d79
Chris Dent <chris.dent@gmail.com>
2014-02-01 17:27:05 +0000

    Add drag and drop file upload
    If you drop a binary into a dropzone in the tank, that file will
    be uploaded (using the name of the file) into the tank in question.
    
    Still to come:
    
    * hiding the interface when not usable
    * showing error message

---
+++ src/tank/filedrop.js
@@ -0,0 +1,64 @@
+$(function() {
+
+    var tank = window.location.pathname.replace(/^\/tanks\//, '').split('/')[0],
+		dropzone = $('#dropzone');
+
+
+    function dragNoop(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+    }
+
+    function handleFile(file) {
+        dropzone.text('Processing ' + file.name);
+
+        var reader = new FileReader();
+        reader.onerror = function(ev) {
+			dropzone.text('Error ' + ev);
+            console.log('error', ev);
+        }
+        reader.onload = function(ev) {
+            var data = ev.target.result.replace(/^data[^,]*,/, '');
+            var tiddler = {
+                tags: [],
+                text: data,
+                title: file.name,
+                type: file.type,
+            };
+			$.ajax({
+				url: '/bags/' + encodeURIComponent(tank) + '/tiddlers/'
+					+ encodeURIComponent(tiddler.title),
+				contentType: 'application/json',
+				type: 'PUT',
+				data: JSON.stringify(tiddler),
+				processData: false,
+				success: function() {
+					dropzone.text('Uploaded ' + file.name);
+				}
+			});
+        }
+
+        reader.onloadend = function(ev) {
+            console.log('onloadend');
+        }
+
+        reader.readAsDataURL(file);
+    }
+
+    function dragDrop(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        var files = ev.originalEvent.dataTransfer.files;
+        var count = files.length;
+        $.each(files, function(index, file) {
+            handleFile(file);
+        });
+    }
+
+    // init event handlers
+    dropzone.on("dragenter", dragNoop);
+    dropzone.on("dragexit", dragNoop);
+    dropzone.on("dragover", dragNoop);
+    dropzone.on("drop", dragDrop);
+});
