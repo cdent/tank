f2e06ab9e9fd9d62cf13505be6e42b9f89225952
Chris Dent <chris.dent@gmail.com>
2014-02-09 18:02:13 +0000

    Given a simple search, provide a link to global search
    That is, if a search has a 'bag:' in it but no AND or OR,
    provide a link to doing the search again, globally.

--- tiddlywebplugins/tank/htmlserialization.py
+++ tiddlywebplugins/tank/htmlserialization.py
@@ -20,11 +20,28 @@ class Serialization(HTMLSerialization):
         if not tiddlers.is_search:
             return HTMLSerialization.list_tiddlers(self, tiddlers)
 
+        search_query = self.environ['tiddlyweb.query'].get('q', [''])[0]
         tiddlers.link = '/search?%s' % self.environ.get('QUERY_STRING', '')
+
+        if 'bag:' in search_query and (
+                'OR' not in search_query and 'AND' not in search_query):
+            global_query = globalize_query(search_query)
+        else:
+            global_query = ''
 
         search_template = get_template(self.environ, SEARCH_TEMPLATE)
         return search_template.generate({
             'gravatar': gravatar(self.environ),
             'user': self.environ['tiddlyweb.usersign']['name'],
-            'tiddlers': tiddlers
+            'tiddlers': tiddlers,
+            'global_query': global_query,
         })
+
+
+def globalize_query(search_query):
+    """
+    Take the bag parts out of a search query.
+    """
+    query = ' '.join([part for part in search_query.split()
+        if not part.startswith('bag:')])
+    return query
