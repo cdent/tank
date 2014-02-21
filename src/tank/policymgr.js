a8f73875a02f6e546d2d00fbd33dda22dbade34b
Chris Dent <chris.dent@gmail.com>
2014-02-21 17:49:38 +0000

    Extract retrieving bag information to a service.
    Thanks to @FND for the promises example at:
       http://fnd.tiddlyspace.com/JavaScript%20Promises
    
    This was initially a bit trickier at first than it should have been.
    Because of the async nature of the http request, sometimes the data
    would not be there when the incoming uri included the bag/tank name.

--- src/tank/policymgr.js
+++ src/tank/policymgr.js
@@ -1,39 +1,70 @@
 
 var app = angular.module('app', []);
 
-/* app.config(function($locationProvider) {
-	$locationProvider.html5Mode(true);
-}); */
-
 app.filter('escape', function() {
 	return window.encodeURIComponent;
 });
 
-app.controller('TanksCtrl', ['$scope', '$http', function($scope, $http) {
-	var currentUser = tiddlyweb.status.username;
+app.factory('tankService', function($http, $q) {
+	var tanks;
+	return {
+		getTanks: function(callback) {
+			if (tanks) {
+				callback(tanks);
+			} else {
+				$http.get('/bags.json?select=policy:create')
+					.then(function(res) {
+						var bags = res.data;
+						var resources = bags.map(function(name) {
+							var uri = '/bags/' + encodeURIComponent(name);
+							return $http.get(uri,
+								{headers: {'Accept': 'application/json'}});
+						});
+						return $q.all(resources);
+					})
+					.then(function(res) {
+						var data = {};
+						res.forEach(function(res) {
+							var tankName = res.config.url
+								.replace(/\/bags\/([^\/]+)/, "$1");
+							angular.extend(res.data, {name: tankName});
+							data[tankName] = res.data;
+						});
+						tanks = data;
+						callback(tanks);
+					});
+			}
+		}
+	};
+});
 
-	$http.get('/bags?select=policy:create',
-		{headers: {'Accept': 'application/json'}})
-		.success(function(data) {
-			$scope.tanks = data;
+app.controller('TanksCtrl', function($scope, tankService) {
+	tankService.getTanks(function(data) {
+		$scope.tanks = data;
 	});
-}]);
+});
 
-app.controller('TankCtrl',
-	['$scope', '$location', '$http', function($scope, $location, $http) {
+app.controller('TankCtrl', function($scope, $location, tankService) {
 	$scope.constraints = ['manage', 'read', 'write', 'create', 'delete'];
+
+	function setData(tankName) {
+		tankService.getTanks(function(data) {
+			$scope.tanks = data;
+			$scope.tank = $scope.tanks[tankName];
+		});
+	}
+
 	$scope.$watch(function() { return $location.path();}, 
 		function(path) {
 			if (path) {
 				var tankName = path.split('/')[1];
 				if (tankName) {
-					$http.get('/bags/' + encodeURIComponent(tankName),
-						{headers: {'Accept': 'application/json'}})
-						.success(function(data) {
-							angular.extend(data, {name: tankName});
-							$scope.tank = data;
-						});
+					setData(tankName);
 				}
 			}
-		});
-}]);
+		}
+	);
+
+	$scope.startEditor = function() {};
+
+});
